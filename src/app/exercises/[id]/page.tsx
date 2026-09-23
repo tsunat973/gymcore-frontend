9aaa881521eb18f41c0d7f4c71a8223fac75ab4e
tsunat973 <t23am018@yamanashi.ac.jp>
2026-09-24 00:54:34 +0900

    記録取得一覧

--- src/app/exercises/[id]/page.tsx
+++ src/app/exercises/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 
-type RecordItemItem = {
+type RecordItem = {
   id: number;
   exercise_id: number;
   date: string;
@@ -26,5 +26,39 @@ export default function RecordPage({
     resolveParams();
   }, [params]);
 
-  return <div>種目ID: {exerciseId}</div>
+  const [records, setRecords] = useState<RecordItem[]>([]);
+
+  const fetchRecords = async () => {
+    const token = localStorage.getItem("token");
+
+    const response = await fetch(
+      `http://localhost:3001/api/records/${exerciseId}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      },
+    );
+
+    const data = await response.json();
+    setRecords(data);
+  };
+
+  useEffect(() => {
+    if (exerciseId) {
+      fetchRecords();
+    }
+  }, [exerciseId]);
+
+  return (
+    <div>
+      <ul>
+        {records.map((record) => {
+          return(
+          <li key={record.id}>
+            {record.date}: {record.weight}kg × {record.reps}回 × {record.sets}
+            セット
+          </li>);
+        })}
+      </ul>
+    </div>
+  );
 }
